1741c93dca31b6341de65083863639cdd83dcdfe
Bryant <bryantgsaunders@gmail.com>
2022-06-21 17:35:36 -0600

    EmailDetector unit tests complete

--- RegularExpressions/src/test/kotlin/TestEmailDetector.kt
+++ RegularExpressions/src/test/kotlin/TestEmailDetector.kt
@@ -1,8 +1,43 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
 
 class TestEmailDetector {
     @Test
-    fun invalidCharacters() {
+    fun testInvalidCharacters() {
+        val detector = EmailDetector()
+        assertFalse(detector.isProperType(""))
+        assertFalse(detector.isProperType(" "))
+    }
 
+    @Test
+    fun testEmailFirstPart() {
+        val detector = EmailDetector()
+        assertFalse(detector.isProperType("@@gmail.com"))
+        assertFalse(detector.isProperType("@ gmail.com"))
+        assertFalse(detector.isProperType("@gmail.com"))
+        assert(detector.isProperType("daksdgbaskhjdb@hotmail.com"))
+        assert(detector.isProperType("khzvkhbvkbh....@hotmail.com"))
+    }
+
+    @Test
+    fun testEmailSecondPart() {
+        val detector = EmailDetector()
+        assertFalse(detector.isProperType("ab@gm.ail.com"))
+        assertFalse(detector.isProperType("ab@gm ail.com"))
+        assertFalse(detector.isProperType("ab@gm@ail.com"))
+        assertFalse(detector.isProperType("ab@.com"))
+        assert(detector.isProperType("daksdgbaskhjdb@hotmail.com"))
+        assert(detector.isProperType("khzvkhbvkbh....@hotmail.com"))
+    }
+
+    @Test
+    fun testEmailThirdPart() {
+        val detector = EmailDetector()
+        assertFalse(detector.isProperType("ab@gmail.co@m"))
+        assertFalse(detector.isProperType("ab@gmail.co m"))
+        assertFalse(detector.isProperType("ab@gmail.co.m"))
+        assertFalse(detector.isProperType("ab@gmail."))
+        assert(detector.isProperType("daksdgbaskhjdb@hotmail.com"))
+        assert(detector.isProperType("khzvkhbvkbh....@hotmail.com"))
     }
 }
